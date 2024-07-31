c185f6ab8535d1f5350094b143b7f28bce6932cf
Muhammed <81745827+M2A2-Muhammed@users.noreply.github.com>
2024-07-31 19:48:40 +0300

    correct cowsay cmd

--- cowsay.sh
+++ cowsay.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 sudo apt-get install cowsay -y
-cowsay -f fragon "Run for cover, I'm Dragon ....RRRR" >> dragon.txt
+cowsay -f dragon "Run for cover, I'm Dragon ....RRRR" >> dragon.txt
 gerp -i "dragon" dragon.txt
 cat dragon.txt
 ls -ltra
